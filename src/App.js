4f9b00609354aa6e0e658df5b60039c4d36f7ae0
curiouscauliflower <131444476+curiouscauliflower@users.noreply.github.com>
2023-08-22 11:31:09 +0300

    Add GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Weather app</h1>
+      <footer>
+        <p>This project was coded by Hanna Tkach and is{" "}
+          <a href="https://github.com/curiouscauliflower/react-weather-application.git" target="_blank" rel="noreferrer noopener nofollow">open-sourced on GitHub</a>
+        </p>
+      </footer>
     </div>
   );
 }
